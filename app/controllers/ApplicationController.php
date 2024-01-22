34ebecefffcf3c3d57d2e234aaf63522331650d5
LemonRH <82542162+LemonRH@users.noreply.github.com>
2024-01-22 14:21:56 +0100

    Update ApplicationController.php

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'TaskListModel.php';
+require_once __DIR__ . '/../models/TaskListModel.php';
 /**
  * Base controller for the application.
  * Add general things in this controller.
